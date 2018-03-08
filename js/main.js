f31a73fe2e89192d8b501d0258e39b259b7b9e26
Ryan DeLosh <ryguy1201@gmail.com>
2018-03-08 13:31:36 -0500

    Added Modal animations

--- js/main.js
+++ js/main.js
@@ -8,6 +8,45 @@ var preloader = bodymovin.loadAnimation({
   name: "Preloader", // Name for future reference. Optional.
 });
 
+// ANIME.JS
+// NEED TO GET WORKING
+// var titleEase = anime({
+//   targets: '#content h1, #content h3, #content hr, #content button',
+//   opacity: 0,
+//   delay: 500,
+//   duration: 1000,
+//   direction: 'reverse',
+//   easing: 'linear'
+// });
+
+var inEase = anime({
+  targets: '#loginModal , #signUpModal',
+  translateY: -500,
+  delay: 10000,
+  duration: 1000,
+  direction: 'reverse',
+  easing: 'easeInCubic'
+});
+
+// WORK IN PROGRESS
+var elInEase = anime({
+  targets: '#loginModal small , #loginModal input , #loginModal label , #signUpModal , #signUpModal input , #signUpModal label',
+  translateX: 200,
+  delay: 1000000,
+  duration: 500,
+  direction: 'reverse',
+  easing: 'easeInCubic'
+})
+
+var outEase = anime({
+  targets: '#loginModal , #signUpModal',
+  translateY: -500,
+  delay: 10000,
+  duration: 1000,
+  direction: 'standard',
+  easing: 'easeInCubic'
+});
+
 // DOCUMENT INFORMATION HAS BEEN RECEIVED
 $(document).ready(function () {
 
@@ -86,25 +125,23 @@ $(document).ready(function () {
     }
   });
 
-// SIGNUP FORM SUBMIT
-  $("#signUpForm").on('submit', function() {
-    $.post('http://localhost:8000/user/signup',
-    {
+  // SIGNUP FORM SUBMIT
+  $("#signUpForm").on('submit', function () {
+    $.post('http://localhost:8000/user/signup', {
       email: $("#signUpForm #email").val(),
       password: $("#signUpForm #pass").val()
     });
   });
 
-// LOGIN FORM SUBMIT
-  $("#loginForm").on('submit', function() {
-    $.post('http://localhost:8000/user/login',
-    {
+  // LOGIN FORM SUBMIT
+  $("#loginForm").on('submit', function () {
+    $.post('http://localhost:8000/user/login', {
       email: $("#loginForm #email").val(),
       password: $("#loginForm #pass").val()
     });
   });
 
-// ADD A SCROLL TO ELEMENT FUNCTION ON CLICK
+  // ADD A SCROLL TO ELEMENT FUNCTION ON CLICK
   jQuery.fn.extend({
     scrollTo: function (speed, easing) {
       return this.each(function () {
@@ -118,6 +155,15 @@ $(document).ready(function () {
 
   $(".scroll").click(function (event) {
     $('#section').scrollTo(900, 'easeInOutQuint');
+  });
+
+  $(".inBut").click(function (event) {
+    inEase.restart();
+    // elInEase.restart();
+  });
+  $(".outBut").click(function (event) {
+    outEase.restart();
+    // elOutEase.restart();
   });
 
 });
